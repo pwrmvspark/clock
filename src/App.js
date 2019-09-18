fe4863ba50f748328edfd5b3f69f8769f30912a3
pwrmvspark <danielparkonline@gmail.com>
2019-09-18 12:39:12 -0700

    testing gif

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 class App extends Component {
   render() {
@@ -8,7 +7,7 @@ class App extends Component {
       <div>
         <Clock />
       </div>
-    );
+    )
   }
 }
 
@@ -17,55 +16,33 @@ class Clock extends Component {
     super()
       this.state = {
         time: new Date()
-      };
+      }
     
     this.tick = this.tick.bind(this)
   }
 
   componentDidMount() {
-    this.timerID = setInterval(this.tick, 1000);
+    this.timerID = setInterval(this.tick, 1000)
   }
   
   componentWillUnmount() {
-    clearInterval(this.timerID);
+    clearInterval(this.timerID)
   }
 
   tick() {
     this.setState({
       time: new Date()
-    });
+    })
   }
 
   render() {
     return (
     <div>
-      <h1>This is the current time sucka!</h1>
-      <h2>It is {this.state.time.toLocaleTimeString()}</h2>
+      <h1 className="header"> Current time using state && component lifecycle methods </h1>
+      <h2 className="header"> It is {this.state.time.toLocaleTimeString()} </h2>
     </div>
     )  
   }
 }
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
